14cfbc4b8e0f0fa1fdfe6310685a39cfe7d1cf04
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-01-04 12:35:31 +0100

    Added string extension to trim and remove multiplicates whitespaces

---
+++ PostidentCheck.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postident.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Trims provided <paramref name="longString"/> and removes any multiplicated white spaces found.
+        /// </summary>
+        /// <param name="longString"><see cref="string"/> to be cleaned</param>
+        /// <returns>A trimmed string with single spaces in place of multiple spaces</returns>
+        public static string RemoveMultiplicatedWhitespaces(this string longString)
+        {
+            _ = longString ?? throw new ArgumentNullException(nameof(longString));
+            StringBuilder sb = new StringBuilder();
+            bool lastWasSpace = true; // True to eliminate leading spaces
+
+            for (int i = 0; i < longString.Length; i++)
+            {
+                if (Char.IsWhiteSpace(longString[i]) && lastWasSpace)
+                {
+                    continue;
+                }
+
+                lastWasSpace = Char.IsWhiteSpace(longString[i]);
+
+                sb.Append(longString[i]);
+            }
+
+            // The last character might be a space
+            if (Char.IsWhiteSpace(sb[sb.Length - 1]))
+            {
+                sb.Remove(sb.Length - 1, 1);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
