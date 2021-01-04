e88f93e314d835dee355d0981235f59662411cee
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-01-04 12:40:15 +0100

    Abstracted main validation service

---
+++ PostidentCheck.Application/Common/Interfaces/IValidationService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Postident.Application.Common.Models;
+using Postident.Core.Entities;
+
+namespace Postident.Application.Common.Interfaces
+{
+    public interface IValidationService
+    {
+        /// <summary>
+        /// Performs offline and online validation of given <paramref name="dataPacks"/>
+        /// </summary>
+        /// <param name="dataPacks">Collection to be validated</param>
+        /// <param name="token">Cancels running validation</param>
+        /// <returns>A collection of <see cref="InfoPackWriteModel"/></returns>
+        Task<IEnumerable<InfoPackWriteModel>> Validate(IEnumerable<DataPack> dataPacks, CancellationToken token);
+    }
+}
