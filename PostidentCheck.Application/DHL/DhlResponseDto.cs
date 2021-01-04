edd17465b3e172441928bc1767d0cca35fd55889
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-01-04 12:45:31 +0100

    Response dto optimization:
    - reworked ToString for more compact output size
    - added auto-filling if some values are empty when trying to generate string
    - added simple IsValid method that checks error code (0 == valid according to API)
    - Error code 0 is also displayed when "weak validation error" is received - API decision

--- PostidentCheck.Application/DHL/DhlResponseDto.cs
+++ PostidentCheck.Application/DHL/DhlResponseDto.cs
@@ -13,21 +13,31 @@ namespace Postident.Application.DHL
         /// </summary>
         public string Key { get; init; } = string.Empty;
 
-        public string ErrorCode { get; init; } = string.Empty;
+        public int ErrorCode { get; init; } = 9999;
 
         public string ErrorText { get; init; } = string.Empty;
 
         public ImmutableHashSet<string> StatusMessages { get; init; } = ImmutableHashSet<string>.Empty;
 
+        /// <summary>
+        /// Validation state according to online service.
+        /// </summary>
+        public bool IsValid => ErrorCode == 0;
+
         public override string ToString()
         {
-            return $"Error code: {ValueOrFiller(ErrorCode)} | " + $"Error text: {ValueOrFiller(ErrorText)} | Status messages(s): {ValueOrFiller(StatusMessages)}";
+            var str = string.Empty;
+            if (!IsValid)
+            {
+                str = $"Error code: {ErrorCode} | Type: {ValueOrFiller(ErrorText)} | ";
+            }
+            return str += $"{ValueOrFiller(StatusMessages)}";
         }
 
         public static implicit operator string(DhlResponseDto response) => response.ToString();
 
         private static string ValueOrFiller(string value) => string.IsNullOrWhiteSpace(value) ? "---" : value;
 
-        private static string ValueOrFiller(ImmutableHashSet<string> values) => values.IsNullOrEmpty() ? "---" : string.Join(" & ", values);
+        private static string ValueOrFiller(ImmutableHashSet<string> values) => values.IsNullOrEmpty() ? "---" : string.Join(" ", values);
     }
 }
