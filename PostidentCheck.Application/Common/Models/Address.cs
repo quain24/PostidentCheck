3b061f145f58c818c0f24d690ac5be56161b543a
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-01-04 12:41:59 +0100

    Added mapped data pack and address objects
    - Data pack will be created from read model - better separation and already processed data
    contained in Address object

---
+++ PostidentCheck.Application/Common/Models/Address.cs
@@ -0,0 +1,25 @@
+﻿namespace Postident.Application.Common.Models
+{
+    public class Address
+    {
+        /// <summary>
+        /// Street name or "Packstation" / "Postfiliale"
+        /// </summary>
+        public string Street { get; init; }
+
+        /// <summary>
+        /// Street number or Packstation / Postfiliale number
+        /// </summary>
+        public string StreetNumber { get; init; } = string.Empty;
+
+        public string Name { get; init; } = "Private";
+        public string PostIdent { get; init; } = string.Empty;
+        public string City { get; init; } = string.Empty;
+        public string ZipCode { get; init; } = string.Empty;
+
+        /// <summary>
+        /// 2-3 letter euro country code (DE)
+        /// </summary>
+        public string CountryCode { get; init; } = string.Empty;
+    }
+}
---
+++ PostidentCheck.Application/Common/Models/DataPack.cs
@@ -0,0 +1,14 @@
+﻿using Postident.Core.Enums;
+
+namespace Postident.Application.Common.Models
+{
+    public class DataPack
+    {
+        public string Id { get; init; }
+        public Carrier Carrier { get; init; }
+
+        public Address Address { get; init; }
+
+        public InfoPackCheckStatus DataPackChecked { get; init; }
+    }
+}
