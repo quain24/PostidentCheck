e93fee88f2bac406909982d671ba62469296e77c
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-01-05 16:07:03 +0100

    Write model now has ToString for clean logging. Cleaned DefaultShipmentValues

--- PostidentCheck.Core/Entities/InfoPackWriteModel.cs
+++ PostidentCheck.Core/Entities/InfoPackWriteModel.cs
@@ -7,5 +7,7 @@ namespace Postident.Core.Entities
         public string Id { get; init; }
         public InfoPackCheckStatus CheckStatus { get; set; }
         public string Message { get; set; }
+
+        public override string ToString() => $"ID: {Id} Check status: {CheckStatus} | Message(s): {Message}";
     }
 }
