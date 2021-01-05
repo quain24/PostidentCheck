c44377d8d6e406a7ea2ab4fade250b2b038069ae
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-01-05 16:03:39 +0100

    Moved and rewritten parser options
    - changed help texts
    - commands are now in lower-case

---
+++ PostidentCheck/ParserOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+using Postident.Core.Enums;
+
+namespace PostidentCheck
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class ParserOptions
+    {
+        [Option('a', "all", Required = false, Default = true, HelpText = "Checks all available data (default)")]
+        public bool CheckAll { get; set; }
+
+        [Option('c', "carrier", Required = false,
+            HelpText = "Checks data only for selected carrier names: '--carrier DPD' or '--carrier DPD DHL'. Accept Carrier numerical codes." +
+                       " Possible options: DHL, DHL_Sperr, DPD, DPD_PL, HES, Schenker, Schenker_PL, Pick_up, Self_delivery", Separator = ',')]
+        public IEnumerable<Carrier> Carriers { get; set; } = Array.Empty<Carrier>();
+
+        [Option('i', "id", Required = false, HelpText = "Checks data only for selected ID's: '--id 1234' or '--id 1234 5678", Separator = ',')]
+        public IEnumerable<string> Ids { get; set; } = Array.Empty<string>();
+    }
+}
