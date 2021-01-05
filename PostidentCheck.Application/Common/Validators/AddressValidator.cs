9f06fdfe451c2007f08a5bd522ae49cc468dde18
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-01-05 08:29:57 +0100

    Address validator modification:
    - Validating street name - cannot be empty
    - Zip code check simplified - international codes need more leeway

--- PostidentCheck.Application/Common/Validators/AddressValidator.cs
+++ PostidentCheck.Application/Common/Validators/AddressValidator.cs
@@ -17,6 +17,10 @@ namespace Postident.Application.Common.Validators
                         $"PostIdent (\"{d.PostIdent}\") is present, but it is not a number.");
             });
 
+            RuleFor(d => d.City)
+                .NotEmpty()
+                .WithMessage("City value is missing");
+
             RuleFor(d => d.CountryCode)
                 .Length(2, 3)
                 .WithMessage("Country code length is out of range (must be between 2-3 chars)")
@@ -26,14 +30,10 @@ namespace Postident.Application.Common.Validators
             When(d => string.IsNullOrWhiteSpace(d.ZipCode) is false, () =>
             {
                 RuleFor(d => d.ZipCode)
-                    .Length(5)
-                    .Must(zip => int.TryParse(zip, out _))
-                    .WithMessage(d => $"Zip code ({d.ZipCode}) is in bad format");
+                    .Length(3, 8)
+                    .WithMessage(d => $"Zip code ({d.ZipCode}) length is not valid (3 - 8 chars)");
             });
 
-            RuleFor(d => d.City)
-                .NotEmpty()
-                .WithMessage("City value is missing");
         }
 
         protected override bool PreValidate(ValidationContext<Address> context, ValidationResult result)
